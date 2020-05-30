3845ef356f6da3e57b3e82cb18fd8c67a92fa560
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-05-29 21:12:01 -0400

    keep track of the interval, need to stop at 0

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import TimeButton from './components/TimeButton';
 import './App.css';
 
 function App() {
-  const [worktime, setWorktime] = useState(25);
+  const [worktime, setWorktime] = useState(1);
   const [breaktime, setBreaktime] = useState(5);
   const [isActive, setIsActive] = useState(false);
 
+  const reset = () => {
+    console.log("Reseting now");
+    setWorktime(25);
+    setIsActive(false);
+  }
+
   return (
     <div className="main-container">
+      <button id="reset" onClick={reset}>Reset</button>
       <DisplayTimer time={worktime} status={isActive} />
       <div className="handler-buttons">
+
+        {/* Session length button */}
         <TimeButton data={{ type: "timer", lengthID: "session-length", label: "Session Length", id: "session-label", dec: "session-decrement", inc: "session-increment" }} minutes={worktime} onClick={setWorktime} />
+
+        {/*Start/ Pause button  */}
         <TimeButton data={{ type: "text", label: "", id: "timer-label" }} status={isActive} text="start" onClick={setIsActive} />
+
+        {/* Break length button */}
         <TimeButton data={{ type: "timer", lengthID: "break-length", label: "Break Length", id: "break-label", dec: "break-decrement", inc: "break-increment" }} minutes={breaktime} onClick={setBreaktime} />
       </div>
     </div>
--- src/components/DisplayTimer.js
+++ src/components/DisplayTimer.js
@@ -2,33 +2,51 @@ import React, { useEffect, useState } from 'react'
 import './DisplayTimer.css';
 
 function DisplayTimer(props) {
-    const [minutes, setMinutes] = useState(0);
+    const [minutes, setMinutes] = useState(() =>
+        props.time
+    );
+
     const [seconds, setSeconds] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
 
     const updateTime = () => {
-        setSeconds(prevState => {
-            if (prevState === 0) {
-                setMinutes(minutes - 1);
+
+        setSeconds(prevSeconds => {
+            if (prevSeconds === 0) {
+                setMinutes(prevMinutes => {
+                    if (prevMinutes === 0) {
+                        clearInterval(intervalId);
+                        console.log("This should stop");
+                        return 0;
+                    }
+                    return prevMinutes - 1
+                });
                 return 59;
             }
-            return prevState - 1;
+            return prevSeconds - 1;
         })
     }
     useEffect(() => {
-        setMinutes(props.time)
-        setSeconds(0);
-        var interval;
         if (props.status) {
-            interval = setInterval(() => {
+            let interval = setInterval(() => {
                 updateTime();
             }, 1000);
+            setIntervalId(interval);
+        } else {
+            clearInterval(intervalId);
         }
-        return () => clearInterval(interval)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props]);
+    }, [props.status]);
+
+    useEffect(() => {
+        console.log("I changed too")
+        setMinutes(props.time);
+        setSeconds(0);
+    }, [props.time]);
     return (
         <div>
-            <span id="time-left" className="timer">{minutes} : {seconds}</span>
+            <span id="time-left" className="timer">
+                {minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds}
+            </span>
         </div>
     )
 }
